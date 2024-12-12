faab05581d90c08bafa118a790cadb5d14d5083f
Yusuf Nasser <yusufnassereng@gmail.com>
2024-12-12 08:34:22 +0200

    Add milestone 3 deliverables

---
+++ Deliverables/Milestone3/MS3 Changed Files/lib/semaphore.c
@@ -0,0 +1,39 @@
+// User-level Semaphore
+
+#include "inc/lib.h"
+
+struct semaphore create_semaphore(char *semaphoreName, uint32 value)
+{
+	//TODO: [PROJECT'24.MS3 - #02] [2] USER-LEVEL SEMAPHORE - create_semaphore
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	panic("create_semaphore is not implemented yet");
+	//Your Code is Here...
+}
+struct semaphore get_semaphore(int32 ownerEnvID, char* semaphoreName)
+{
+	//TODO: [PROJECT'24.MS3 - #03] [2] USER-LEVEL SEMAPHORE - get_semaphore
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	panic("get_semaphore is not implemented yet");
+	//Your Code is Here...
+}
+
+void wait_semaphore(struct semaphore sem)
+{
+	//TODO: [PROJECT'24.MS3 - #04] [2] USER-LEVEL SEMAPHORE - wait_semaphore
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	panic("wait_semaphore is not implemented yet");
+	//Your Code is Here...
+}
+
+void signal_semaphore(struct semaphore sem)
+{
+	//TODO: [PROJECT'24.MS3 - #05] [2] USER-LEVEL SEMAPHORE - signal_semaphore
+	//COMMENT THE FOLLOWING LINE BEFORE START CODING
+	panic("signal_semaphore is not implemented yet");
+	//Your Code is Here...
+}
+
+int semaphore_count(struct semaphore sem)
+{
+	return sem.semdata->count;
+}
